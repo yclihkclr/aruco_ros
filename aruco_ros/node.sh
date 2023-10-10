3ecbae27f27f8a67362e97496af9c928ad377502
jkzhao01 <zhaojunkai1515@gmail.com>
2023-10-10 22:55:04 +0800

    node.sh source

---
+++ aruco_ros/node.sh
@@ -0,0 +1,5 @@
+conda deactivate
+source devel/setup.bash
+# export PYTHONPATH=$PYTHONPATH:/home/hkclr/realtime_motion_planning/simulation:/opt/ros/noetic/lib/python3/dist-packages:/home/hkclr/realtime_motion_planning/devel/lib/python3/dist-packages
+export ROS_MASTER_URI=http://192.168.1.8:11311
+export ROS_IP=192.168.1.5
